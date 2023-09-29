025fd7c5f41e0ac8917182b8d0f8aca96b50c9ef
Dealer86 <adicroitor86@yahoo.com>
2023-09-30 00:38:30 +0300

    Added fetch_local_events and newsletter functions in views.py

--- event_newsletter_project/newsletter/views.py
+++ event_newsletter_project/newsletter/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from .models import Event
+import requests
 
-# Create your views here.
+
+# Function to fetch local weekend events from the SerpApi Google Events API
+def fetch_local_events():
+    api_key = "YOUR_SERPAPI_API_KEY"  # Replace with your actual SerpApi API key
+    api_url = (
+        f"https://serpapi.com/google-events-api?q=weekend%20events&api_key={api_key}"
+    )
+
+    try:
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            events_data = response.json().get("events", [])
+            return events_data
+        else:
+            print(f"Failed to fetch events. Status code: {response.status_code}")
+            return []
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching events: {e}")
+        return []
+
+
+# View to display the newsletter
+def newsletter(request):
+    # Fetch local weekend events
+    events = fetch_local_events()
+
+    # Query the Event model to get existing events from the database
+    database_events = Event.objects.all()
+
+    # Combine the fetched API events with database events
+    combined_events = events + list(database_events)
+
+    return render(request, "newsletter/newsletter.html", {"events": combined_events})

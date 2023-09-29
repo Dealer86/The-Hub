e9b2b74af0789f6fc14994f5b7c794e890c16c33
Dealer86 <adicroitor86@yahoo.com>
2023-09-30 00:37:02 +0300

    Created models and applied changes for Event

--- event_newsletter_project/newsletter/models.py
+++ event_newsletter_project/newsletter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title

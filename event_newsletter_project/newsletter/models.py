519909e6e1befe9dc5e52999f01cbd92a7dfebd1
Dealer86 <adicroitor86@yahoo.com>
2023-09-30 16:41:01 +0300

    Added models for Event with title, date, address and link that our class EventHub needs and made migrations and migrate to apply them.

--- event_newsletter_project/newsletter/models.py
+++ event_newsletter_project/newsletter/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=255)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    location = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField(max_length=200, default="Unknown Title")
+    date = models.CharField(max_length=100, default="Unknown Date")
+    address = models.CharField(max_length=200, default="Unknown Address")
+    link = models.URLField(default="Unknown Link")
 
     def __str__(self):
         return self.title

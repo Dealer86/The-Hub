88212caded665d8373434065f448c4b023d9de19
Dealer86 <adicroitor86@yahoo.com>
2023-09-30 16:42:37 +0300

    Added views.py with POST or GET for EventsHub

--- event_newsletter_project/newsletter/views.py
+++ event_newsletter_project/newsletter/views.py
@@ -1,43 +1,34 @@
 from django.shortcuts import render
+from django.contrib import messages  # Import messages framework
+from .forms import EventsHubForm
+from .local_events import EventsHub
 from .models import Event
-import requests
-
-
-# Function to fetch local weekend events from the SerpWow service
-def fetch_local_events():
-    url = "https://serpapi.com/search?engine=google_events"
-
-    payload = {
-        "api_key": "ADI coder nebun ce esti, replace with real SERPI API_KEY",
-        "engine": "google",
-        "q": "Events in Sibiu",
-    }
-    response = requests.get(url, params=payload)
-    data = response.json()
-    events_data = []
-    if data:
-        for event in data["events_results"]:
-            a_dict = {
-                "title": event["title"],
-                "date": event["date"],
-                "address": event["address"],
-                "link": event["link"],
-            }
-            events_data.append(a_dict)
-    else:
-        return []
-    return events_data
 
 
 # View to display the newsletter
 def newsletter(request):
-    # Fetch local weekend events
-    events = fetch_local_events()
+    if request.method == "POST":
+        form = EventsHubForm(request.POST)
+        if form.is_valid():
+            try:
+                city_location = form.cleaned_data["city_location"]
+                events_hub = EventsHub(city_location)
+                fetch_local_events = events_hub.fetch_local_events()
+                Event.objects.all().delete()
+                for event in fetch_local_events.itertuples():
+                    Event.objects.create(
+                        title=event.title,
+                        date=event.date,
+                        address=event.address,
+                        link=event.link,
+                    )
+            except Exception as e:
+                messages.error(request, f"Reason: {str(e)}")
+    else:
+        form = EventsHubForm()
 
-    # Query the Event model to get existing events from the database
     database_events = Event.objects.all()
 
-    # Combine the fetched API events with database events
-    combined_events = events + list(database_events)
-
-    return render(request, "newsletter/newsletter.html", {"events": combined_events})
+    return render(
+        request, "newsletter/newsletter.html", {"form": form, "events": database_events}
+    )

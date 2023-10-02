b161dcdaf904e52a5c7a30475ac9bd4d13f08eb2
Dealer86 <adicroitor86@yahoo.com>
2023-10-02 15:54:05 +0300

    Added a media file configuration for handling media(in case we need one).Now it's commented

--- event_newsletter_project/event_newsletter_project/settings.py
+++ event_newsletter_project/event_newsletter_project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Ensure that Django project's settings have proper configurations for handling media files
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")

1c597278c5faf1bcaf76e3098777aa021dec78b6
Dealer86 <adicroitor86@yahoo.com>
2023-10-02 15:56:59 +0300

    Added image and description to views.py

--- event_newsletter_project/newsletter/views.py
+++ event_newsletter_project/newsletter/views.py
@@ -5,22 +5,24 @@ from .local_events import EventsHub
 from .models import Event
 
 
-# View to display the newsletter
+# View to display the World Events
 def newsletter(request):
     if request.method == "POST":
         form = EventsHubForm(request.POST)
         if form.is_valid():
             try:
-                city_location = form.cleaned_data["city_location"]
+                city_location = form.cleaned_data["google_events_search_query"]
                 events_hub = EventsHub(city_location)
                 fetch_local_events = events_hub.fetch_local_events()
                 Event.objects.all().delete()
                 for event in fetch_local_events.itertuples():
                     Event.objects.create(
                         title=event.title,
+                        image=event.image,
                         date=event.date,
                         address=event.address,
                         link=event.link,
+                        description=event.description,
                     )
             except Exception as e:
                 messages.error(request, f"Reason: {str(e)}")

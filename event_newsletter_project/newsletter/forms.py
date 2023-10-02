026e1a73c47faface682773e6cebd5075be95680
Dealer86 <adicroitor86@yahoo.com>
2023-10-02 15:49:35 +0300

    Added placeholder for forms.py

--- event_newsletter_project/newsletter/forms.py
+++ event_newsletter_project/newsletter/forms.py
@@ -2,4 +2,12 @@ from django import forms
 
 
 class EventsHubForm(forms.Form):
-    city_location = forms.CharField(max_length=100)
+    google_events_search_query = forms.CharField(
+        label="Search Events",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": 'Get started, enter your event location name and/or category. Ex: "Sibiu", "concerts in Chicago"',
+            }
+        ),
+        max_length=255,
+    )

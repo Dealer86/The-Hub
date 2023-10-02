ad6302aa82003b61bfa6690a2505a1b3641af2aa
Dealer86 <adicroitor86@yahoo.com>
2023-10-02 15:51:56 +0300

    Added image and description to models.py and changed the max_length

--- event_newsletter_project/newsletter/models.py
+++ event_newsletter_project/newsletter/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=200, default="Unknown Title")
+    title = models.CharField(max_length=400, default="Unknown Title")
+    image = models.URLField(default="No image")
     date = models.CharField(max_length=100, default="Unknown Date")
     address = models.CharField(max_length=200, default="Unknown Address")
     link = models.URLField(default="Unknown Link")
+    description = models.CharField(max_length=300, default="Description not available")
 
     def __str__(self):
         return self.title

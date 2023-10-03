7b7f306652189ccb81096f98b2253d30c3960820
Dealer86 <adicroitor86@yahoo.com>
2023-10-03 14:24:39 +0300

    Added tests for URL location, URL names, correct templates used and correct content display by templates.

--- event_newsletter_project/events_app/tests.py
+++ event_newsletter_project/events_app/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# TestCase its used if it has a database
+# SimpleTestCase is used if we do not have a database
+# TransactionTestCase is helpful to directly test database transactions
+# LiveServerTestCase launches a live server thread for testing with browser based tools like Selenium
+# Reverse is used to test the URL name for each page/route
+
+
+class EventHubViewsTests(TestCase):
+    # test URL location
+    def test_url_exists_at_the_correct_location(self):
+        #  If we have another route then localhost like about you must end it with a slash /about/
+        #  To run test quit server if running and python manage.py test
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    # test URL names
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("event_hub_views"))
+        self.assertEqual(response.status_code, 200)
+
+    # test that the correct templates are used on each page
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("event_hub_views"))
+        self.assertTemplateUsed(response, "events_app/events_app.html")
+
+    # test that they display the expected content
+    def test_template_content(self):
+        response = self.client.get(reverse("event_hub_views"))
+        self.assertContains(response, "<title>Events Hub</title>")
+
+    # You can use this to abide by DRY
+    # def __get_response(self):
+    #     response = self.client.get(reverse("event_hub_views"))
+    #     return response
